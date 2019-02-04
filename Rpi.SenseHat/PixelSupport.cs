a1a2b341cb404e5baaebd4892e64a83822ddc3bb
DanielSSilva <daniel.sobral.silva.9@gmail.com>
2019-02-04 22:03:23 +0000

    test

---
+++ Rpi.SenseHat/PixelSupport.cs
@@ -0,0 +1,208 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+//  This file is part of Rpi.SenseHat
+//
+//  Copyright (c) 2017, Mattias Larsson
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of 
+//  this software and associated documentation files (the "Software"), to deal in 
+//  the Software without restriction, including without limitation the rights to use, 
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+//  Software, and to permit persons to whom the Software is furnished to do so, 
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all 
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+//  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using Image = SixLabors.ImageSharp.Image;
+
+namespace Emmellsoft.IoT.Rpi.SenseHat
+{
+	public static class PixelSupport
+	{
+		/// <summary>
+		/// Gets a 2-dimensional pixel array from an image.
+		/// </summary>
+		/// <param name="imageUri">The URI to the image.</param>
+		public static Color[,] GetPixels(Uri imageUri)
+		{
+			Color[,] pixels = null;
+            
+            using (FileStream stream = File.OpenRead(imageUri.AbsolutePath))
+		    using (Image<Rgba32> image = Image.Load(stream))
+		    {
+		        var pixelData = image.SavePixelData();
+
+		        pixels = new Color[image.Width, image.Height];
+
+                int pixelDataIndex = 0;
+                for (int y = 0; y < image.Height; y++)
+		        {
+		            for (int x = 0; x < image.Width; x++)
+		            {
+		                byte b = pixelData[pixelDataIndex];
+		                byte g = pixelData[pixelDataIndex + 1];
+		                byte r = pixelData[pixelDataIndex + 2];
+		                byte a = pixelData[pixelDataIndex + 3];
+
+		                pixels[x, y] = Color.FromArgb(a, r, g, b);
+
+		                pixelDataIndex += 4;
+		            }
+		        }
+            }
+            
+			return pixels;
+		}
+
+		internal static void ConvertDirectionParameters(
+			DisplayDirection direction,
+			bool flipHorizontal,
+			bool flipVertical,
+			out bool leftToRight,
+			out bool topToBottom,
+			out bool flipAxis)
+		{
+			switch (direction)
+			{
+				case DisplayDirection.Deg0:
+					if (!flipHorizontal && !flipVertical)
+					{
+						leftToRight = true;
+						topToBottom = true;
+					}
+					else if (flipHorizontal && !flipVertical)
+					{
+						leftToRight = false;
+						topToBottom = true;
+					}
+					else if (!flipHorizontal /* && flipVertical */)
+					{
+						leftToRight = true;
+						topToBottom = false;
+					}
+					else /* if (flipHorizontal && flipVertical) */
+					{
+						leftToRight = false;
+						topToBottom = false;
+					}
+
+					flipAxis = false;
+					break;
+
+				case DisplayDirection.Deg90:
+					if (!flipHorizontal && !flipVertical)
+					{
+						leftToRight = false;
+						topToBottom = true;
+					}
+					else if (flipHorizontal && !flipVertical)
+					{
+						leftToRight = true;
+						topToBottom = true;
+					}
+					else if (!flipHorizontal /* && flipVertical */)
+					{
+						leftToRight = false;
+						topToBottom = false;
+					}
+					else /* if (flipHorizontal && flipVertical) */
+					{
+						leftToRight = true;
+						topToBottom = false;
+					}
+
+					flipAxis = true;
+					break;
+
+				case DisplayDirection.Deg180:
+					if (!flipHorizontal && !flipVertical)
+					{
+						leftToRight = false;
+						topToBottom = false;
+					}
+					else if (flipHorizontal && !flipVertical)
+					{
+						leftToRight = true;
+						topToBottom = false;
+					}
+					else if (!flipHorizontal /* && flipVertical */)
+					{
+						leftToRight = false;
+						topToBottom = true;
+					}
+					else /* if (flipHorizontal && flipVertical) */
+					{
+						leftToRight = true;
+						topToBottom = true;
+					}
+
+					flipAxis = false;
+					break;
+
+				case DisplayDirection.Deg270:
+					if (!flipHorizontal && !flipVertical)
+					{
+						leftToRight = true;
+						topToBottom = false;
+					}
+					else if (flipHorizontal && !flipVertical)
+					{
+						leftToRight = false;
+						topToBottom = false;
+					}
+					else if (!flipHorizontal /* && flipVertical */)
+					{
+						leftToRight = true;
+						topToBottom = true;
+					}
+					else /* if (flipHorizontal && flipVertical) */
+					{
+						leftToRight = false;
+						topToBottom = true;
+					}
+
+					flipAxis = true;
+					break;
+
+				default:
+					throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+			}
+		}
+
+		public static Color[,] Convert1DTo2D(Color[] pixels)
+		{
+			if (pixels.Length != 64)
+			{
+				throw new ArgumentException("The pixel array must be 64 bytes long (i.e. 8x8).", nameof(pixels));
+			}
+
+			var pixels2D = new Color[8, 8];
+
+			int i = 0;
+			for (int y = 0; y < 8; y++)
+			{
+				for (int x = 0; x < 8; x++)
+				{
+					pixels2D[x, y] = pixels[i++];
+				}
+			}
+
+			return pixels2D;
+		}
+	}
+}
---
+++ Rpi.SenseHat/SenseHatFactory.cs
@@ -0,0 +1,97 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+//  This file is part of Rpi.SenseHat
+//
+//  Copyright (c) 2017, Mattias Larsson
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of 
+//  this software and associated documentation files (the "Software"), to deal in 
+//  the Software without restriction, including without limitation the rights to use, 
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+//  Software, and to permit persons to whom the Software is furnished to do so, 
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all 
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+//  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System.Threading.Tasks;
+using RichardsTech.Sensors;
+using RichardsTech.Sensors.Devices.HTS221;
+using RichardsTech.Sensors.Devices.LPS25H;
+using RichardsTech.Sensors.Devices.LSM9DS1;
+using Unosquare.RaspberryIO;
+
+namespace Emmellsoft.IoT.Rpi.SenseHat
+{
+    /// <summary>
+    /// Factory for creating the ISenseHat object.
+    /// </summary>
+    public static class SenseHatFactory
+	{
+		private const byte DeviceAddress = 0x46;
+
+		private static readonly Task<ISenseHat> _getSenseHatTask = GetSenseHatTask();
+
+		/// <summary>
+		/// Creates the SenseHat object.
+		/// </summary>
+		public static Task<ISenseHat> GetSenseHat()
+		{
+			return _getSenseHatTask;
+		}
+
+		private static async Task<ISenseHat> GetSenseHatTask()
+		{
+			MainI2CDevice mainI2CDevice = CreateDisplayJoystickI2CDevice();
+
+			ImuSensor imuSensor = await CreateImuSensor().ConfigureAwait(false);
+
+			PressureSensor pressureSensor = await CreatePressureSensor().ConfigureAwait(false);
+
+			HumiditySensor humiditySensor = await CreateHumiditySensor().ConfigureAwait(false);
+
+			return new SenseHat(mainI2CDevice, imuSensor, pressureSensor, humiditySensor);
+		}
+
+		private static MainI2CDevice CreateDisplayJoystickI2CDevice()
+		{
+		    var device = Pi.I2C.AddDevice(0x10);
+            return new MainI2CDevice(device);
+		}
+
+		private static async Task<ImuSensor> CreateImuSensor()
+		{
+			var lsm9Ds1Config = new LSM9DS1Config();
+
+			var imuSensor = new LSM9DS1ImuSensor(
+				LSM9DS1Defines.ADDRESS0,
+				LSM9DS1Defines.MAG_ADDRESS0,
+				lsm9Ds1Config,
+				new SensorFusionRTQF());
+
+			await imuSensor.InitAsync().ConfigureAwait(false);
+			return imuSensor;
+		}
+
+		private static async Task<PressureSensor> CreatePressureSensor()
+		{
+			var pressureSensor = new LPS25HPressureSensor(LPS25HDefines.ADDRESS0);
+			await pressureSensor.InitAsync().ConfigureAwait(false);
+			return pressureSensor;
+		}
+
+		private static async Task<HumiditySensor> CreateHumiditySensor()
+		{
+			var humiditySensor = new HTS221HumiditySensor(HTS221Defines.ADDRESS);
+			await humiditySensor.InitAsync().ConfigureAwait(false);
+			return humiditySensor;
+		}
+	}
+}
